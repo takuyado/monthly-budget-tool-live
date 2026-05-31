dc92f96bc01a5b781e3070721e9483951e602a42
Hank Yang <hy@NightClaw.local>
2026-05-31 15:57:08 +0800

    Deploy service worker update fix

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/monthly-budget-tool-live/sw.js', { scope: '/monthly-budget-tool-live/' })})}
